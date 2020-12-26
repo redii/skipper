e41305139207ff0031bbd7547f31540ab780cecf
akmann <akmann.henry@gmail.com>
2020-12-26 21:06:25 +0000

    20201226

---
+++ hugo/content/itadm/server/_index.md
@@ -0,0 +1,6 @@
+---
+title: "Serversysteme und Monitoring"
+date: 2020-12-20T19:49:07Z
+draft: false
+---
+
--- scripts/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Helper script to setup the docker-compose structure
-
-cd $(dirname $0)
-./setup_caddy.sh
-./setup_youtrack.sh
-./setup_minecraft.sh
